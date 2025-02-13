b01fb2ab822b886503e9d4979a0f70ef6983c48e
axeldiazdevivar11 <axeldiazdevivar@MacBook-Air-de-Axel.local>
2025-02-12 19:04:31 -0600

    add actividad 4

---
+++ swift/Actividad4/Operadores.swift
@@ -0,0 +1,31 @@
+print("ASSIGMENT OPERATOR")
+let b = 10
+var a = 5
+a = b
+print("El valor de a es \(a)")
+
+print("Tupla")
+let (x,y) = (1,2)
+print("x es \(x) y y es \(y)")
+
+print("ARITHMETIC OPERATORS")
+1 + 2 print("1 + 2 =", 1 + 2) 
+5 - 3 print("5 - 3 =", 5 - 3)
+print("2 * 3 ="  , 2 * 3)
+print("10.0 / 2.5 =", 10.0 * 2.5)
+
+print("COMPOUND ASSIGMENT")
+var myScore = 10
+print("Valor inicial de myscore: \(myScore)")
+myScore += 3
+print("myScore += 3: \(myScore)")
+myScore -= 5
+print("myScore -= 5: \(myScore)")
+myScore *= 2
+print("myScore *= 2: \(myScore)")
+myScore /= 2
+print("myScore /= 2: \(myScore)")
+
+
+print("REMINDER OPERATOR")
+let dividend = 10 let divisor = 3
